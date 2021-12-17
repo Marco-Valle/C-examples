d9fe692c1741dd822055c06a20c5906635393edc
Marco Valle <marcovalle13@gmail.com>
2021-12-17 14:29:11 +0100

    Update example01.c

--- example01.c
+++ example01.c
@@ -2,199 +2,201 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LEN 25
+
 /*
  * Past examination 18/02/2016 (version D)
  * today date: 12/03/2021
  * my signature: 49dbffdad5428e0a32ba850a6e5e0d45a550c70ef110a748058feb78590ca4dd
 */
 
-FILE* mapFile;
-FILE* findsFile;
-FILE* outFile;
-float** map;
-float** finds;
-unsigned N = 0; // The size of the map NxN
+typedef struct {
+    unsigned row;
+    unsigned column;
+    float weight;
+} Find;
 
 unsigned importMatrix(FILE*, float***);
-unsigned recoverFinds(unsigned, unsigned, unsigned);
+unsigned importFinds(FILE*, Find**);
+unsigned recoverFinds(Find*, float**, float, float, unsigned, unsigned);
 void printUsage();
-void registerFind(unsigned, unsigned, unsigned, unsigned, float, float, float);
+void my_open(FILE**, char*);
+void *my_malloc(size_t);
 
 int main(int argc, char **argv) {
 
-    // MAIN VARIABLES
-    char mapName[25];
-    char findName[25];
-    const char outName[] = "out.txt";
-    unsigned maxPressure;
-    unsigned maxWeight;
-    unsigned numberOfFinds;
-    int fileErr; // the error number when we open the file (we need this later)
+    Find *finds;
+    float **map, maxPressure, maxWeight;
+    unsigned numberOfFinds, N;
+    FILE *mapFile = NULL, *findsFile = NULL;
+    char mapName[MAX_LEN], findName[MAX_LEN];
 
-    if(argc==5){
-        strcpy_s(mapName, sizeof(mapName), argv[1]);
-        strcpy_s(findName, sizeof(findName), argv[2]);
-        maxPressure = (unsigned) argv[3];
-        maxWeight = (unsigned) argv[4];
+    if(argc >= 5){
+        strcpy_s(mapName, _countof(mapName) * sizeof(char), argv[1]);
+        strcpy_s(findName, _countof(findName) * sizeof(char), argv[2]);
+        sscanf_s(argv[3], "%f", &maxPressure);
+        sscanf_s(argv[4], "%f", &maxWeight);
     }else{
-        // Some default values
-        strcpy_s(mapName, sizeof(mapName), "SEA_map.txt");
-        strcpy_s(findName, sizeof(findName), "finds.txt");
-        maxPressure = 120;
-        maxWeight = 40;
         printUsage();
+        exit(EXIT_SUCCESS);
     }
 
     // Open the files
-    if((fileErr = fopen_s(&mapFile, mapName, "r")) != 0)
-        return fileErr;
-    if((fileErr = fopen_s(&findsFile, findName, "r")) != 0)
-        return fileErr;
-    if((fileErr = fopen_s(&outFile, outName, "w")) != 0)
-        return fileErr;
-
-    // Print the header in the output file
-    fprintf_s(outFile, "find\tdive\tx\ty\tpressure\tweight\t\tactualWeight\n");
+    my_open(&mapFile, mapName);
+    my_open(&findsFile, findName);
 
     // Import the map
-    N = importMatrix(mapFile, &map);
-    if(N>0)
-        printf("A %dX%d map imported\n", N, N);
-    else
-        return 1;
+    if((N = importMatrix(mapFile, &map))) {
+        printf("A %uX%u map imported\n", N, N);
+    } else {
+        printf("Can't import the map. Abort.\n");
+        exit(EXIT_FAILURE);
+    }
 
     // Import the finds
-    if((numberOfFinds = importMatrix(findsFile, &finds)) == 0)
+    if((numberOfFinds = importFinds(findsFile, &finds))) {
+        printf("%u finds in the area\n", numberOfFinds);
+    } else {
         printf("No finds in the area\n");
-    else
-        printf("%d finds in the area\n", numberOfFinds);
+    }
 
     // Start the recovering operation
-    if (numberOfFinds)
-        printf("\n%d finds recovered\n", recoverFinds(maxPressure, maxWeight, numberOfFinds));
-
-    // Close the files
-    _fcloseall();
+    if (numberOfFinds) {
+        printf("\n%u finds recovered\n", recoverFinds(finds, map, maxPressure, maxWeight, numberOfFinds, N));
+    }
 
     // Deallocate memory
     free(map);
     free(finds);
 
-    return 0;
+    exit(EXIT_SUCCESS);
 }
 
 void printUsage(){
     printf("\nUsage: ./program mapName findsName maxPressure maxWeight\n\n");
 }
 
-unsigned importMatrix(FILE* file, float*** matrix){
+unsigned importMatrix(FILE* file, float ***matrix){
 
-    /*
-     * This function read char by char the file and import the float values
-     * when it finds a white space it casts the number and add a column
-     * when it find the line feed add a new row in the matrix
-     * it returns the number of the rows
-     */
+    unsigned N = 1;
+    float **ptr;
+    int c;
 
-    unsigned Y = 0;
-    unsigned X = 0;
-    unsigned tmpPosition = 0;
-    unsigned stop = 0;
-    unsigned i = 0;
-    char c;
-    char* tmp;
+    // Get the size
+    rewind(file);
+    while ((c = fgetc(file)) != 10 || c == EOF){
+        if (c == 32){
+            N++;
+        }
+    }
+    rewind(file);
 
-    // Clean the matrix and allocate some new space
-    free(*matrix);
-    *matrix = malloc(sizeof(float));
-    (*matrix)[Y] = malloc(sizeof(float));
-    // Allocate some space also for the tmp variable
-    tmp = malloc(sizeof(char));
+    // Allocate the memory
+    ptr = (float **) my_malloc(sizeof(float*) * N);
+    *matrix = ptr;
+    for (int i = 0; i < N; ++i) {
+        ptr[i] = my_malloc(sizeof(float) * N);
+    }
 
-    rewind(file); // precaution
-    while (!feof(file) || !stop){
-        if((c = fgetc(file)) == NULL || c==-1)
-            stop = 1; // save the last value and then stop
-        if(c==32 || c==10 || c==-1){
-            // Add to the matrix the float
-            (*matrix)[Y][X] = (float) atof(tmp);
-            // Clean tmp array
-            tmpPosition = 0;
-            free(tmp);
-            tmp = malloc(sizeof(char));
-            switch (c) {
-                case 32: // space
-                    // Resize the row of the matrix
-                    X++;
-                    (*matrix)[Y] = realloc((*matrix)[Y], (X+1)*sizeof(float));
-                    break;
-                case 10: // line feed
-                    Y++;
-                    X = 0;
-                    (*matrix) = realloc((*matrix), (Y+1)*sizeof(float));
-                    (*matrix)[Y] = malloc(sizeof(float));
-                    i++;
-                    break;
-                default:
-                    break;
-            }
-        }else{
-            tmp[tmpPosition] = c;
-            tmpPosition++;
-            tmp = realloc(tmp, (tmpPosition+1)*sizeof(char));
+    // Import the values
+    for (int i = 0; i < N; ++i) {
+        if (!fscanf_s(file, "%f%*c", &ptr[i / N][i % N])){
+            printf("\nMap file with a wrong format. Abort.");
+            exit(EXIT_FAILURE);
         }
     }
 
-    // The last one is not counted
-    if (i > 0)
-        i++;
-
-    free(tmp);
-
-    return i;
+    return N;
 }
 
-unsigned recoverFinds(unsigned maxPressure, unsigned maxWeight, unsigned numberOfFinds){
+unsigned importFinds(FILE *file, Find **finds){
 
-    unsigned x, y; // coordinates
+    unsigned N = 1;
+    int c;
+    Find *ptr;
+
+    // Get the size
+    rewind(file);
+    while ((c = fgetc(file)) != EOF){
+        if (c == 10) N++;
+    }
+    rewind(file);
+
+    // Allocate the memory
+    ptr = (Find *) my_malloc(sizeof(Find) * N);
+    *finds = ptr;
+
+    // Import the values
+    for (int i = 0; i < N; ++i) {
+        if (fscanf_s(file, "%u%*c%u%*c%f%*c", &ptr[i].row, &ptr[i].column, &ptr[i].weight) != 3){
+            printf("\nFinds file with a wrong format. Abort.");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return N;
+}
+
+unsigned recoverFinds(Find *finds,
+                      float **map,
+                      float maxPressure,
+                      float maxWeight,
+                      unsigned numberOfFinds,
+                      unsigned N){
+
     unsigned dives = 1;
     unsigned findsCaught = 0;
     float actualWeight = 0;
 
-    for(unsigned i = 0; i<numberOfFinds; i++){
+    for(unsigned i = 0; i < numberOfFinds; i++){
         printf("\n");
-        // Check if the find's weight isn't over the maximum weight transportable
-        if (finds[i][2] > maxWeight)
-            printf("Error: too much weight\n\tDive:\t%d\n\tFind:\t%d\n\tWeight:\t%f\n",dives, i, finds[i][2]);
-        else {
-            x = (unsigned) finds[i][0];
-            y = (unsigned) finds[i][1];
 
-            // Check if the find is in the map
-            if (x>=N || y>=N)
-                printf("Error: out of the sea\n\tDive:\t%d\n\tFind:\t%d\n\tX:\t%d\n\tY:\t%d\n",dives, i, x, y);
-            else{
-                // Check the pressure
-                if (map[x][y] > maxPressure)
-                    printf("Error: too much pressure\n\tDive:\t\t%d\n\tFind:\t\t%d\n\tPressure:\t%.*f\n",dives, i, 2, map[x][y]);
-                else {
-                    actualWeight += finds[i][2];
-                    // If we are over the max weight, empty the cargo and carry again the find
-                    if (actualWeight > maxWeight) {
-                        actualWeight = finds[i][2];
-                        printf("\nNew dive\n");
-                        dives++;
-                    }
-                    findsCaught++;
-                    printf("Find caught\n\tDive:\t\t%d\n\tActual weight:\t%.*f\n\tFind:\t\t%d\n\tX:\t\t%d\n\tY:\t\t%d\n", dives, 2, actualWeight, i, x, y);
-                    registerFind(i, dives, x, y, map[x][y], finds[i][2], actualWeight);
-                }
-            }
+        // Check if the find's weight isn't over the maximum weight transportable
+        if (finds[i].weight > maxWeight) {
+            printf("Error: too much weight\n\tDive:\t%u\n\tFind:\t%u\n\tWeight:\t%.2f\n", dives, i, finds[i].weight);
+            goto end;
         }
+
+        // Check if the find is in the map
+        if (finds[i].column >= N || finds[i].row >= N){
+            printf("Error: out of the sea\n\tDive:\t%u\n\tFind:\t%u\n\tX:\t%u\n\tY:\t%u\n",dives, i, finds[i].column, finds[i].row);
+            goto end;
+        }
+
+        // Check the pressure
+        if (map[ finds[i].row ][ finds[i].column ] > maxPressure){
+            printf("Error: too much pressure\n\tDive:\t\t%u\n\tFind:\t\t%u\n\tPressure:\t%.2f\n", dives, i, map[ finds[i].row ][ finds[i].column ]);
+            goto end;
+        }
+
+        actualWeight += finds[i].weight;
+        // If we are over the max weight, empty the cargo and carry again the find
+        if (actualWeight > maxWeight) {
+            actualWeight = finds[i].weight;
+            printf("\nNew dive\n");
+            dives++;
+        }
+
+        findsCaught++;
+        printf("Find caught\n\tDive:\t\t%d\n\tActual weight:\t%.2f\n\tFind:\t\t%d\n\tX:\t\t%d\n\tY:\t\t%d\n",
+               dives, actualWeight, i, finds[i].column, finds[i].row);
+
+        end:;
     }
     return findsCaught;
 }
 
-void registerFind(unsigned find, unsigned dive, unsigned x, unsigned y, float pressure, float weight, float actualWeight){
-    fprintf_s(outFile, "%d\t%d\t%d\t%d\t%.*f\t\t%.*f\t\t%.*f\n", find, dive, x, y, 2, pressure, 2, weight, 2, actualWeight);
+void my_open(FILE **fp, char *filename){
+    if(fopen_s(fp, filename, "r")) {
+        exit(EXIT_FAILURE);
+    }
+}
+
+void *my_malloc(size_t size){
+    void *ptr = malloc(size);
+    if (ptr == NULL){
+        printf("\nMemory allocation error. Abort.");
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
 }
